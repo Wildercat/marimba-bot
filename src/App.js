fc73d0b9dedb7d4b1a435c5be82c02950c34d1f4
Wildercat <abramgornik@gmail.com>
2019-11-21 16:19:58 -0500

    create, store and play songs

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  useParams
 } from "react-router-dom";
 
 class App extends React.Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      song: null
+    }
+  }
 
   async createHistory(song_id) {
     let response;
@@ -44,8 +50,66 @@ class App extends React.Component {
     console.log(response.data);
   }
 
+  async getSong(song_id) {
+    let response;
+    try {
+      response = await axios.get('http://127.0.0.1:8000/api/songs/' + song_id)
+    } catch (error) {
+      // Error 
+      if (error.response) {
+        /*
+         * The request was made and the server responded with a
+         * status code that falls out of the range of 2xx
+         */
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        /*
+         * The request was made but no response was received, `error.request`
+         * is an instance of XMLHttpRequest in the browser and an instance
+         * of http.ClientRequest in Node.js
+         */
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request and triggered an Error
+        console.log('Error', error.message);
+      }
+      console.log(error);
+    }
+    console.log(response.data);
+    this.setState({ song: JSON.parse(response.data.data) });
+  }
+
+  initArray(width, height) {
+    let arr = [];
+    for (let idx = 0; idx < height; idx++) {
+      let jrr = [];
+      for (let jdx = 0; jdx < width; jdx++) {
+        jrr.push(0);
+      }
+      arr.push(jrr);
+    }
+    // console.log(arr);
+    return arr;
+  }
+
+  // songViewer() {
+  //   console.log(this.state.song);
+  //   let { song_id } = useParams();
+  //   console.log(song_id);
+  //   if (this.state.song) {
+  //     return (
+  //       <Song song_id={null} array={this.state.song} />
+  //     )
+  //   } else {
+  //     this.getSong(song_id);
+  //     return <h1>loading</h1>
+  //   }
+  // }
+
   render() {
-    this.createHistory(43);
+    // this.createHistory(43);
     return (
       <Router>
         <div>
@@ -66,9 +130,13 @@ class App extends React.Component {
           {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
           <Switch>
-            <Route path="/">
-              <Song width={5} height={16} />
+            {/* <Route path="/songs/53">
+              {this.songViewer(53)}
+            </Route> */}
+            <Route exact path="/">
+              <Song song_id={null} array={this.initArray(5, 16)} />
             </Route>
+            {/* <Route path="/:id" children={this.songViewer()} /> */}
           </Switch>
         </div>
       </Router>
--- src/Song.js
+++ src/Song.js
@@ -7,44 +7,122 @@ class Song extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            arr: this.initArray()
+            arr: this.props.array,
+            song_id: this.props.song_id
         }
+        this.handleSubmitSong = this.handleSubmitSong.bind(this);
+        this.handlePlaySong = this.handlePlaySong.bind(this);
     }
 
-    handleClick(i,j) {
-        console.log({i,j});
-        let arr = this.state.arr;
-        arr[i][j] = !arr[i][j];
-        this.setState({arr: arr});
-    }
-    initArray() {
-        let width = this.props.width;
-        let height = this.props.height;
-
-        let arr = [];
-        for (let idx = 0; idx < height; idx++) {
-            let jrr = [];
-            for (let jdx = 0; jdx < width; jdx++) {
-                jrr.push(0);
+    async createSong(song_array) {
+        let response;
+        const post = { data: JSON.stringify(song_array) }
+        try {
+            response = await axios.post('http://127.0.0.1:8000/api/songs', post)
+        } catch (error) {
+            // Error 
+            if (error.response) {
+                /*
+                 * The request was made and the server responded with a
+                 * status code that falls out of the range of 2xx
+                 */
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                /*
+                 * The request was made but no response was received, `error.request`
+                 * is an instance of XMLHttpRequest in the browser and an instance
+                 * of http.ClientRequest in Node.js
+                 */
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request and triggered an Error
+                console.log('Error', error.message);
             }
-            arr.push(jrr);
+            console.log(error);
         }
-        console.log(arr);
-        return arr;
+        console.log(response.data);
+        this.setState({song_id: response.data.id})
     }
-    componentDidMount() {
-        this.setState({arr: this.initArray()})
+
+    handleClick(i, j) {
+        console.log({ i, j });
+        let arr = this.state.arr;
+        arr[i][j] = arr[i][j] ? 0 : 1;
+        this.setState({ arr: arr });
     }
+
+
+    handleSubmitSong() {
+        console.log(this.state.arr);
+        this.createSong(this.state.arr);
+    }
+
+    async createHistory() {
+        let response;
+        const post = { song_id: this.state.song_id }
+        try {
+            response = await axios.post('http://127.0.0.1:8000/api/history', post)
+        } catch (error) {
+            // Error 
+            if (error.response) {
+                /*
+                 * The request was made and the server responded with a
+                 * status code that falls out of the range of 2xx
+                 */
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                /*
+                 * The request was made but no response was received, `error.request`
+                 * is an instance of XMLHttpRequest in the browser and an instance
+                 * of http.ClientRequest in Node.js
+                 */
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request and triggered an Error
+                console.log('Error', error.message);
+            }
+            console.log(error);
+        }
+        console.log(response.data);
+    }
+
+    handlePlaySong() {
+        this.createHistory();
+    }
+
+    playButton() {
+        return this.state.song_id ? (
+            <button onClick={this.handlePlaySong} type='button' className="btn btn-danger">Play Song</button>
+        ) : null;
+    }
+
+    // PlayButton() {
+    //     const id = this.state.song_id;
+    //     return id ? (
+    //         <p>{id}</p>
+    //     ) : (
+
+    //     );
+    // }
+
     render() {
         const Renderedarray = this.state.arr;
 
         return (
             <>
-            <p>songhere</p>
-            <Grid onClick={(i, j) => this.handleClick(i, j)} array={Renderedarray} size={2} />
+                <p>songhere</p>
+                <Grid onClick={(i, j) => this.handleClick(i, j)} array={Renderedarray} size={2} />
+                <button onClick={this.handleSubmitSong} type="button" className="btn btn-primary">Save Song</button>
+                {this.playButton()}
+                {/* <button onClick={this.handlePlaySong} type='button' className="btn btn-danger">Play Song</button> */}
             </>
         );
     }
 }
  
+
 export default Song;
