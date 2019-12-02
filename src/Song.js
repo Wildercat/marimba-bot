84a1cbddf79c0223d1934bc628858cf7577abcfe
Wildercat <abramgornik@gmail.com>
2019-12-02 12:05:21 -0500

    merge song saving and song playing into one button

--- src/Song.js
+++ src/Song.js
@@ -67,10 +67,16 @@ class Song extends React.Component {
         this.setState({ arr: arr });
     }
 
+    async submitAndPlay() {
+        await this.createSong(this.state.arr);
+        this.createHistory();
+    }
+
 
     handleSubmitSong() {
-        console.log(this.state.arr);
-        this.createSong(this.state.arr);
+        this.submitAndPlay();
+        // console.log(this.state.arr);
+        // this.createSong(this.state.arr);
     }
 
     async createHistory() {
@@ -106,12 +112,6 @@ class Song extends React.Component {
 
     handlePlaySong() {
         this.createHistory();
-    }
-
-    playButton() {
-        return this.state.song_id ? (
-            <button onClick={this.handlePlaySong} type='button' className="btn btn-danger">Play Song</button>
-        ) : null;
     }
 
     // --------- Visual/Styling -----------
@@ -174,42 +174,6 @@ class Song extends React.Component {
         Tone.Transport.start();
     }
 
-    exampleSequence() {
-        //setup a polyphonic sampler
-        var keys = new Tone.Players({
-            "A": "./audio/casio/A1.[mp3|ogg]",
-            "C#": "./audio/casio/Cs2.[mp3|ogg]",
-            "E": "./audio/casio/E2.[mp3|ogg]",
-            "F#": "./audio/casio/Fs2.[mp3|ogg]",
-        }, {
-            "volume": -10,
-            "fadeOut": "64n",
-        }).toMaster();
-        //the notes
-        var noteNames = ["F#", "E", "C#", "A"];
-        var loop = new Tone.Sequence(function (time, col) {
-            var column = document.querySelector("tone-step-sequencer").currentColumn;
-            column.forEach(function (val, i) {
-                if (val) {
-                    //slightly randomized velocities
-                    var vel = Math.random() * 0.5 + 0.5;
-                    keys.get(noteNames[i]).start(time, 0, "32n", 0, vel);
-                }
-            });
-            //set the columne on the correct draw frame
-            // Tone.Draw.schedule(function(){
-            // 	document.querySelector("tone-step-sequencer").setAttribute("highlight", col);
-            // }, time);
-        }, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], "16n").start(0);
-        //bind the interface
-        // document.querySelector("tone-transport").bind(Tone.Transport);
-        // Tone.Transport.on("stop", () => {
-        // 	setTimeout(() => {
-        // 		document.querySelector("tone-step-sequencer").setAttribute("highlight", "-1");
-        // 	}, 100);
-        // });
-    }
-
     handleStopSong() {
         Tone.Transport.stop();
     }
@@ -231,7 +195,6 @@ class Song extends React.Component {
                     <button onClick={this.handleSubmitSong} type="button" className="btn btn-danger">
                         Play Song
                     </button>
-                    {this.playButton()}
                 </div>
 
             </>
